8cfda9d0b0cb56a6ae2b86a7bfbee38ea1c33666
Donghyun <dev.dhson@gmail.com>
2023-07-07 11:31:23 +0900

    세팅 및 기본 구현

---
+++ src/main/java/com/ll/dynamodb_2023/DynamoDb2023Application.java
@@ -0,0 +1,13 @@
+package com.ll.dynamodb_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDb2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamoDb2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/dynamodb_2023/DynamoDb2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.dynamodb_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDb2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
